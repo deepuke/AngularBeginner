91e25a0277853a9df60e9e39f291ddd8cb0f24b0
Deepu Elayakunju <deepuke77@gmail.com>
2015-08-21 22:41:10 +0530

    added project dependencies

---
+++ app/app.js
@@ -0,0 +1,11 @@
+angular.module("app", []).controller("myCtrl", function($scope){
+	$scope.arr = [{
+		name : "Deepu",
+		age : 31,
+		place : "USA"
+	},{
+		name : "Anisha",
+		age : 61,
+		place : "kollam"
+	}];
+});
